22b4e728ed1078f1a99b53309195ac04bb8676ca
PabloFigue <125218736+PabloFigue@users.noreply.github.com>
2023-03-20 22:59:02 -0600

    Actualización Proyecto

--- MPLAB.X/nbproject/Makefile-variables.mk
+++ MPLAB.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Hola_Mundo.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Hola_Mundo.X.production.hex
+CND_ARTIFACT_NAME_default=MPLAB.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MPLAB.X.production.hex
